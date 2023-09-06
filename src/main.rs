8252331d67d556493d390ba10ea06e27209a6127
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2023-09-06 15:32:58 +0200

    fixup! Initial rust boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use std::error::Error;
-use serde::Deserialize;
-use magick_rust::{MagickWand, magick_wand_genesis};
-use magick_rust::bindings::ImageType_GrayscaleType;
+use image2::*;
+fn main() -> Result<(), Error> {
 
-#[derive(Deserialize)]
-pub struct Cat {
-    pub url: String
-}
+    let image = Image::<f32, image2::Rgb>::open("in.jpg")?;
+    let conv = filter::convert();
+    let dest: Image<f32, Gray> = image.run(conv, None);
+    dest.save("out.jpg")?;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    magick_wand_genesis();
-    let mut wand = MagickWand::new();
-    let cats = reqwest::blocking::get("https://api.thecatapi.com/v1/images/search")?
-        .json::<Vec<Cat>>()?;
-    let cat_blob = reqwest::blocking::get(&cats[0].url)?
-        .bytes()?;
-    wand.read_image_blob(cat_blob)?;
-    wand.set_image_type(ImageType_GrayscaleType)?;
-    wand.write_image("cat.jpg")?;
     Ok(())
 }
